05c2805198a0b79fbd819e5f216418661fdff57f
SINGHPREETAMKUMAR <126652092+SINGHPREETAMKUMAR@users.noreply.github.com>
2023-05-22 14:32:19 +0530

    Add files via upload

---
+++ module1 sdlc.py
@@ -0,0 +1,25 @@
+Python Assignment
+Module – 1 (SDLC)
+1) What is Software?
+ans.
+Software is a a set of instructions, data or programs used to operate computers and execute specific tasks.
+What are the types of Applications?
+ANS. 
+1. Web Development
+2. Machine Learning and Artificial Intelligence
+3. Data Science
+4. Game Development
+5. Audio and Visual Applications
+6. Software Development
+7. CAD Applications
+8. Business Applications
+9. Desktop GUI
+10. Web Scraping Application
+3)What is programing?
+
+ANS. Programming is the process or activity of writing computer programs.
+4)What is Python?
+
+ANS. Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis.
+
+
